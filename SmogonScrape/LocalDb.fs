e174d78eb915edb8809d81412a605c3c07071706
Isaac C. Aronson <i@pingas.org>
2020-01-11 16:53:33 -0500

    Import local legit pokemon

---
+++ SmogonScrape/LocalDb.fs
@@ -0,0 +1,39 @@
+﻿module LocalDb
+open System.IO
+type PKM = PKHeX.Core.PK8
+// Desktop PKHeX pkdb folder
+[<Literal>]
+let localPath = "C:\Users\i\Desktop\PKHex\pkmdb"
+
+let localDirectory = new DirectoryInfo(localPath)
+
+// FileInfos for each .pkm8 file
+let localFileInfos = localDirectory.EnumerateFiles()
+
+// All files as Seq
+let pokemonFileSequence : FileInfo seq =
+    localFileInfos
+    |> Seq.cast<FileInfo>
+
+// Raw legit pokemon data type
+type LegitPokemon = LegitPokemon of PKM
+// Function to parse raw file data into PKMN class
+let getPKMN (file:FileInfo) =
+    new PKM(File.ReadAllBytes(file.FullName))
+    |> LegitPokemon
+// Construct Seq of LegitPokemon
+let localPokemon =
+    pokemonFileSequence
+    |> Seq.map getPKMN
+
+// Legal Pokemon data type
+type LegalPokemon = LegalPokemon of PKM
+// Raise to legal pokemon if it passes the validator
+let makeLegal pokemon : LegalPokemon option =
+    let (LegitPokemon legitPokemon) = pokemon
+    let analysis = new PKHeX.Core.LegalityAnalysis(legitPokemon)
+    if analysis.Valid then Some (LegalPokemon legitPokemon)
+    else None
+// All local, legal, legit pokemon
+let legalPokemon : LegalPokemon seq =
+    Seq.choose makeLegal localPokemon
---
+++ SmogonScrape/Program.fs
@@ -0,0 +1,8 @@
+﻿// Learn more about F# at https://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open LocalDb
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    let foo = LocalDb.legalPokemon
+    0 // return an integer exit code
